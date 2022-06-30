f654c2359386e6d5e471a23847f27198ed54267d
Igor Osipov <igor.osipov@ottofeller.com>
2022-07-01 00:05:45 +0300

    refactor: make widget update if some of app props changed

--- src/ReactWidget.tsx
+++ src/ReactWidget.tsx
@@ -1,19 +1,64 @@
 import { AppProps } from 'types'
 import worldId from 'index'
 import { validateInputParams } from 'utils'
-import { useEffect, useRef, useState } from 'preact/hooks'
+import { useEffect, useRef } from 'preact/hooks'
 
-export function ReactWidget(
-  props: AppProps & {
-    className?: string
-    onSuccess?: () => void
-    onError?: () => void
-    enabled?: boolean
-    debug?: boolean
-  }
-): JSX.Element {
+type Props = AppProps & {
+  className?: string
+  onSuccess?: () => void
+  onError?: () => void
+  enabled?: boolean
+  debug?: boolean
+}
+
+const additionalPropsKeys = ['className', 'onSuccess', 'onError', 'enabled', 'debug'] as const
+type AppPropsKeys = keyof AppProps
+
+export function ReactWidget(props: Props): JSX.Element {
   const widgetReference = useRef<HTMLDivElement | null>(null)
-  const [validation, setValidation] = useState<ReturnType<typeof validateInputParams>>({ valid: false })
+  const validation = validateInputParams(props)
+  const prevAppProps = useRef<AppProps | null>(null)
+
+  const getAppProps = (currentProps: Props) => {
+    const propsKeys = Object.keys(currentProps) as Array<keyof Props>
+
+    return propsKeys.reduce(
+      (accumulator: AppProps, currentValue) => {
+        if (additionalPropsKeys.some((key) => key === currentValue)) {
+          return accumulator
+        }
+
+        return {
+          ...accumulator,
+          [currentValue as AppPropsKeys]: props[currentValue],
+        }
+      },
+      { action_id: '' }
+    )
+  }
+
+  const getChangedAppProps = (appProps: AppProps) => {
+    if (!prevAppProps.current) {
+      return null
+    }
+
+    if (JSON.stringify(appProps) === JSON.stringify(prevAppProps.current)) {
+      return null
+    }
+
+    const appPropsKeys = Object.keys(appProps) as Array<AppPropsKeys>
+
+    return appPropsKeys.reduce((accumulator: { [K in AppPropsKeys]?: AppProps[K] }, currentValue) => {
+      if (prevAppProps.current?.[currentValue] && prevAppProps.current?.[currentValue] === appProps[currentValue]) {
+        return accumulator
+      }
+
+      return {
+        ...accumulator,
+        [currentValue]: appProps[currentValue],
+      }
+    }, {})
+  }
 
   function activateWorldId() {
     worldId
@@ -41,23 +86,36 @@ export function ReactWidget(
   }
 
   useEffect(() => {
-    setValidation(validateInputParams(props))
-
-    if (!validation.valid || !widgetReference.current || worldId.isEnabled()) {
+    if (!widgetReference.current) {
       return
     }
 
-    worldId.init(widgetReference.current, {
-      enable_telemetry: props.enable_telemetry || false,
-      action_id: props.action_id,
-      signal: props.signal,
-      app_name: props.app_name,
-      signal_description: props.signal_description,
-      theme: props.theme,
-    })
+    if (!validation.valid && props.debug) {
+      console.log('Props validation error: ', validation)
+    }
 
-    // REVIEW should we use isInitialized to enable worldID?
-    if (worldId.isInitialized() && props.enabled) {
+    if (!worldId.isInitialized()) {
+      worldId.init(widgetReference.current, {
+        enable_telemetry: props.enable_telemetry || false,
+        action_id: props.action_id,
+        signal: props.signal,
+        app_name: props.app_name,
+        signal_description: props.signal_description,
+        theme: props.theme,
+      })
+    }
+
+    const appProps = getAppProps(props)
+    const changedProps = getChangedAppProps(appProps)
+    prevAppProps.current = appProps
+
+    if (worldId.isInitialized() && changedProps) {
+      worldId.update({
+        ...changedProps,
+      })
+    }
+
+    if (worldId.isInitialized() && !worldId.isEnabled() && props.enabled) {
       activateWorldId()
     }
   }, [props, validation])
