03406544a2d0363be4b7eed8a2d4b1a346101f34
michaelfurutin <104859710+michaelfurutin@users.noreply.github.com>
2022-07-05 23:35:41 +0400

    style: update ui (#25)
    Co-authored-by: Paolo D'Amico <paolodamico@users.noreply.github.com>

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -36,7 +36,7 @@ const SLoader = styled.div`
 
 export const Loader = (): JSX.Element => (
   <SLoader>
-    <svg width="32" height="32" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="1em" height="1em" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="17.9997" cy="17.9997" r="15.9997" stroke="currentColor" stroke-width="4" />
       <path
         d="M33.9981 17.9997C33.9981 15.3386 33.3343 12.7194 32.0668 10.3795C30.7994 8.03951 28.9683 6.05265 26.7394 4.59879C24.5104 3.14492 21.9541 2.26998 19.3017 2.05319C16.6494 1.83639 13.9849 2.28459 11.5495 3.3572C9.11404 4.42981 6.9846 6.09295 5.35399 8.19602C3.72338 10.2991 2.64309 12.7757 2.21094 15.4015"
