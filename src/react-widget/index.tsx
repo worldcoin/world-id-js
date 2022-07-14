a46721457e3f8426f1a4ec3796c107c1cac025ae
Igor Osipov <igor.osipov@ottofeller.com>
2022-07-14 14:37:25 +0300

    feat: add development mode ui

--- src/react-widget/index.tsx
+++ src/react-widget/index.tsx
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Container>
-      <Widget />
+      <Widget theme="dark" />
     </Container>
   </React.StrictMode>
 )
