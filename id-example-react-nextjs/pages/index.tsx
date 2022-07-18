8a6ce0386907297477aa405682b24750121da9c1
Michael Furutin <michael.furutin@ottofeller.com>
2022-07-18 21:25:40 +0300

    feat: add react-widget to example nextjs app

--- id-example-react-nextjs/pages/index.tsx
+++ id-example-react-nextjs/pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import dynamic from 'next/dynamic'
+
+const Widget = dynamic(() =>
+  import("@worldcoin/id").then((mod) => mod.Widget), { ssr: false }
+);
+
+import { AppProps } from "@worldcoin/id/dist/types/app-props";
+
+const widgetProps: AppProps = {
+  connectionProps: {
+    enable_telemetry: true,
+    action_id: "wid_staging_PCNQeDC5CX",
+    signal: "user-id-1",
+    app_name: "candyApp",
+    signal_description: "Receive initial airdrop April 2022",
+    onVerificationError: ({ code, detail }) => console.log({ code, detail }),
+    onVerificationSuccess: (result) => console.log(result),
+  },
+  theme: "light",
+  debug: true,
+  onInitSuccess: () => console.log("Init successful"),
+  onInitError: ({ error }) =>
+    console.log("Error while initialization World ID", error),
+};
 
 const Home: NextPage = () => {
   return (
@@ -13,58 +36,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Widget {...widgetProps} />
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
