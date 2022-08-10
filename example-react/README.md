a5ee2b4aad58169ca09d88826b3f3ac4afc8c9da
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-08-10 09:18:09 +0100

    update readme & pkg version

--- example-react/README.md
+++ example-react/README.md
@@ -1,1 +1,12 @@
-# React Example
+# World ID JS - React Example
+
+This package contains a very simple example of how to use the World ID JS widget on a React app. The repository was scaffolded using `create-react-app`.
+
+To run this example simply run:
+
+```sh
+# make sure to run this in the `/example-react` folder
+npm run dev
+```
+
+More information about the JS widget can be found [here](../README.md).
--- example-react/src/App.tsx
+++ example-react/src/App.tsx
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { WorldIDWidget, utils, WidgetProps } from "@worldcoin/id";
+import { WorldIDWidget, WidgetProps } from "@worldcoin/id";
 
 const widgetProps: WidgetProps = {
   actionId: "wid_staging_PCNQeDC5CX",
@@ -9,7 +9,7 @@ const widgetProps: WidgetProps = {
   appName: "candyApp",
   signalDescription: "Receive initial airdrop April 2022",
   theme: "light",
-  debug: true,
+  debug: true, // DO NOT SET TO `true` IN PRODUCTION
   onSuccess: (result) => console.log(result),
   onError: ({ code, detail }) => console.log({ code, detail }),
   onInitSuccess: () => console.log("Init successful"),
@@ -22,11 +22,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>World ID React Example</p>
-        {/* ADD REACT COMPONENT HERE */}
+        {/* World ID component below */}
         <WorldIDWidget {...widgetProps} />
-        <div
-          style={{ marginTop: "16px" }}
-        >{`Random number from utils ${utils.randomNumber(1, 100)}`}</div>
       </header>
     </div>
   );
