15cd8a6bc6f905b8c771c3f5ad00919225776217
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-09-01 13:03:55 +0200

    theme

--- id/src/stories/ConfirmedScene.stories.tsx
+++ id/src/stories/ConfirmedScene.stories.tsx
@@ -1,17 +1,36 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { ConfirmedScene } from 'scenes/ConfirmedScene'
-import { globalStyles } from 'stitches'
+import { globalStyles, lightTheme, darkTheme } from 'stitches'
+
+// FIXME: Abstract this into a reusable component for theming config
+interface ArgsInterface {
+  theme: 'light' | 'dark'
+}
 
 export default {
   title: 'Widget',
   component: ConfirmedScene,
+  argTypes: {
+    theme: {
+      control: {
+        type: 'select',
+        options: ['light', 'dark'],
+      },
+    },
+  },
+  args: {
+    theme: 'light',
+  },
 } as ComponentMeta<typeof ConfirmedScene>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof ConfirmedScene> = () => {
+const Template: ComponentStory<typeof ConfirmedScene> = (args) => {
   globalStyles()
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
+      className={(args as ArgsInterface).theme === 'light' ? lightTheme : darkTheme}
+    >
       <ConfirmedScene />
     </div>
   )
